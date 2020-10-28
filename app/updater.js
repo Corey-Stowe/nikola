1fa97950bc8427007116819c2a7a2e5344b20e12
BadAimWeeb <29724654+lequanglam@users.noreply.github.com>
2020-10-28 20:13:28 +0700

    Beta version updater

--- app/updater.js
+++ app/updater.js
@@ -18,11 +18,11 @@ function concatStream(stream) {
     });
 }
 
-function spawn(cmd, arg) {
+function spawn(cmd, arg, stdio = "pipe") {
     return new Promise(resolve => {
         var npmProcess = childProcess.spawn(cmd, arg, {
             shell: true,
-            stdio: "pipe",
+            stdio,
             cwd: process.cwd()
         });
         npmProcess.on("close", function (code) {
@@ -124,7 +124,50 @@ module.exports = class Updater extends events.EventEmitter {
     }
 
     async performUpdate() {
-        this.#logFunc("Update logic isn't implented yet.");
-        return false;
+        let gitCheck = await spawn("git", ["rev-parse", "--is-inside-work-tree"]);
+        var isGit = (!gitCheck[0] ? gitCheck[1].toString("utf8").split(/(\r\n)|(\r)|(\n)/g)[0] == "true" : false);
+
+        if (isGit) {
+            // Use git to get updates
+            try {
+                let pull1 = await spawn("git", ["pull"]);
+                if (pull1[0] === 0) throw true;
+                await spawn("git", ["reset", "--hard"]);
+                let pull2 = await spawn("git", ["pull"]);
+                if (pull2[0] === 0) throw true;
+                let currECode = pull2[0];
+                if (pull2[0] === 128) {
+                    await spawn("git", ["config", "user.name", "NikolaUpdater"]);
+                    await spawn("git", ["config", "user.email", "autoupdate@nikola.bot"]);
+                    let pull3 = await spawn("git", ["pull"]);
+                    currECode = pull3[0];
+                }
+                if (currECode === 0) throw true;
+                await spawn("git", ["fetch", "origin", "master"]);
+                let reset = await spawn("git", ["reset", "--hard", "origin/master"]);
+                if (reset[0] !== 0) {
+                    this.#logFunc(`ERROR: Cannot restore HEAD to latest. (Error code ${reset[0]})`);
+                    throw false;
+                }
+                throw true;
+            } catch (e) {
+                if (e === true) {
+                    await fs.promises.writeFile(path.join(process.cwd(), ".data", "flag_update_npm"));
+                    let updateConfig = require("./updater.updateConfig");
+                    if (await updateConfig()) {
+                        return true;
+                    }
+                    this.#logFunc("ERROR: Cannot update config to latest version. Please copy .env.default file to .env and change your config.");
+                    return false;
+                } else {
+                    return false;
+                }
+            }
+            
+        } else {
+            // Use fetch to get updates from GitHub
+            this.#logFunc("Stable version updater is not implented yet.")
+            return false;
+        }
     }
 };
---
+++ app/updater.updateConfig.js
@@ -0,0 +1,32 @@
+let fs = require("fs");
+let path = require("path");
+let os = require("os");
+
+const replaceTable = [{}];
+
+module.exports = async function updateConfig() {
+    // Get .env.default file
+    let defaultENV = await fs.promises.readFile(path.join(process.cwd(), ".env.default"), { encoding: "utf8" });
+    let splitedDefENV = defaultENV.split(/$/m).map(x => x.replace(/\r|\n/g, "")).filter((_, i) => i % 2 === 0);
+    let updatedDefENV = splitedDefENV.map(v => {
+        if (v.startsWith("#")) return v;
+        let splitedKeyValue = v.split("=", 2);
+        
+        let newValue = process.env[splitedKeyValue[0]] || splitedKeyValue[1];
+
+        let repTable = replaceTable[parseInt(process.env.CONFIG_VERSION)][splitedKeyValue[0]];
+        if (repTable && repTable[newValue]) {
+            newValue = repTable[newValue];
+        }
+
+        return splitedKeyValue[0] + "=" + newValue;
+    });
+
+    let newENV = updatedDefENV.join(os.EOL);
+    try {
+        await fs.promises.writeFile(path.join(process.cwd(), ".env"), newENV, { encoding: "utf8" });
+    } catch (ex) {
+        return false;
+    }
+    return true;
+}
